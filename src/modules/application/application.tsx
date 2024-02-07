8fd4873beccca2267a15602512ed93f2853dc0fb
Vegard Merkesvik <veme001@student.kristiania.no>
2024-02-07 14:08:13 +0100

    Added the map and context

---
+++ src/modules/application/application.tsx
@@ -0,0 +1,44 @@
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+
+import "./application.css";
+import "ol/ol.css";
+import { map, MapContext } from "../map/mapContext";
+import { Layer } from "ol/layer";
+
+export function Application() {
+  function handleFocusUser(e: React.MouseEvent) {
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const { latitude, longitude } = pos.coords;
+      map.getView().animate({
+        center: [longitude, latitude],
+        zoom: 12,
+      });
+    });
+  }
+
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+  ]);
+  useEffect(() => map.setLayers(layers), [layers]);
+
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  useEffect(() => map.setTarget(mapRef.current), []);
+  return (
+    <MapContext.Provider value={{ map, layers, setLayers }}>
+      <header>
+        <h1>Kommune kart</h1>
+      </header>
+      <nav>
+        <a href={"#"} onClick={handleFocusUser}>
+          Focus on me
+        </a>
+      </nav>
+      <main>
+        <div ref={mapRef}></div>
+      </main>
+    </MapContext.Provider>
+  );
+}
