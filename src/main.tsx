2c86a769fed8fb02a7117c29cf149fe693dd949d
Vegard Merkesvik <veme001@student.kristiania.no>
2024-02-07 14:09:32 +0100

    prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Application} from "./modules/application/application";
+import { Application } from "./modules/application/application";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<Application/>);
+root.render(<Application />);
